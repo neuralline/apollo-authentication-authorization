def88b3c4e3dd2b482048d757f4b7c6c6b56c012
NeuralLine <neurallines>
2021-01-28 02:23:11 +0000

    added phone into authenticated rules

--- src/providers/authenticatedFields.ts
+++ src/providers/authenticatedFields.ts
@@ -11,8 +11,15 @@ const authenticatedFields = (
   currentUser: IUser
 ): IUser[] => {
   try {
+    //set up rules for authenticated Fields
+    //returns null if the user is not authenticated
     const rule =
-      currentUser.role === 'USER' ? {role: null, department: null} : {}
+      currentUser.role === 'USER'
+        ? {role: null, department: null, phone: null}
+        : {}
+
+    //apply rules
+    //loops through each and check for fields
     const serialisedFields: IUser[] = users.map((user: SUser) => {
       return {id: user._id, ...user._doc, ...rule}
     })
